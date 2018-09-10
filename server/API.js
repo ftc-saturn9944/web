8e4c4cfb642ecefbe222b1b2b388a1f51d28ba1a
Ricky LeDew <adotout1@gmail.com>
2018-09-10 17:49:39 -0400

    improved comments

--- server/API.js
+++ server/API.js
@@ -1,20 +1,22 @@
-const express = require('express');
+const express = require('express'); //Express - Provides a server backend
 
-const compression = require('compression');
-const bodyParser = require('body-parser');
+const compression = require('compression'); //Compression - Automatically compresses data we send
+const bodyParser = require('body-parser');  //Body Parser - Allows us to access data in JSON we recieve
 const fileUpload = require('express-fileupload');
-const morgan = require('morgan')
+const morgan = require('morgan') //Morgan - Server Logger
 
 const app = express();
 
+//Use the middleware that we want
 app.use(compression());
 app.use(bodyParser({ limit: '50mb' }));
 app.use(fileUpload());
 app.use(morgan('combined'))
 
+// Serve static files from the React app
 app.use(express.static('www'));
 
-// Serve static files from the React app
+//Set up route for the url at /api/hello, and send a string as a response.
 app.get('/api/hello', (req, res) => {
     res.status(200).send('Hello SATURN');
 });
@@ -25,6 +27,7 @@ app.get('*', (req, res) => {
     res.sendFile('www/index.html');
 });
 
+//Listen for traffic on port 5000
 const port = process.env.PORT || 5000;
 app.listen(port);
 

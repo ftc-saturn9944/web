35b967049a4fbbee4ed06116e406661f05dffb1a
Breanna <breannaaylesbury3@gmail.com>
2018-09-15 16:58:29 -0400

    Breanna's Change

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -14,6 +14,7 @@ class Main extends Component {
                     <li>Sofia</li>
                     <li>Vincent</li>
                     <li>Billy</li>
+                    <li>Breanna</li>
                 </ul>
             </div>
         );

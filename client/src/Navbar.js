45a191ab551c85f60bce3fd148e53d971173b807
Ricky LeDew <adotout1@gmail.com>
2018-09-15 17:29:15 -0400

    navbar component #2

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -4,7 +4,9 @@ class Navbar extends Component {
     render() {
         return (
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-                <a class="navbar-brand" href="#">Navbar</a>
+                <a class="navbar-brand" href="#">
+                    <img src="/images/saturn-logo.png" />
+                </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>

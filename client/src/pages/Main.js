110a22bf96db9a4ca07fe0972b7445695ff4ca29
Michael Privat <mprivat@majorspot.com>
2018-09-14 20:54:11 -0400

    Bootstrap setup

---
+++ client/src/pages/Main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+    render() {
+        return (
+            <div className="App">
+                Future home of team SATURN...
+                <p>Yeah... We've got work to do...</p>
+            </div>
+        );
+    }
+}
+
+export default Main;

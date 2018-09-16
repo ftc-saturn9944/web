d55711a7097688942b77f3b069cbdf9140350351
Michael Privat <mprivat@majorspot.com>
2018-09-15 20:19:47 -0400

    React component for scalable carousel

--- client/src/pages/TeamInfo.js
+++ client/src/pages/TeamInfo.js
@@ -3,31 +3,34 @@ import Navbar from '../Navbar';
 
 class TeamInfo extends Component {
     render() {
+        const images = ["meet1.jpg", "meet1.jpg", "meet1.jpg", "meet1.jpg", "meet1.jpg"];
+
+        const carouselIndicators = [];
+        const imageComponents = [];
+        images.forEach((name, index) => {
+            carouselIndicators.push(<li data-target="#carouselComponent" data-slide-to={index} key={index} className={index == 0 ? "active" : "other"}></li>);
+            imageComponents.push(
+                <div className={`carousel-item${index == 0 ? " active" : ""}`}>
+                    <img className="d-block mx-auto" src={`/images/${name}`} max-width="100%" max-height="100%" />
+                </div>
+            );
+        });
+
         return (
             <div className="App">
                 <Navbar />
-                <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+                <div id="carouselComponent" className="carousel slide" data-ride="carousel">
                     <ol className="carousel-indicators">
-                        <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                        {carouselIndicators}
                     </ol>
                     <div className="carousel-inner">
-                        <div className="carousel-item active">
-                            <img className="d-block w-100" src="/images/meet1.jpg" alt="First slide" />
-                        </div>
-                        <div className="carousel-item">
-                            <img className="d-block w-100" src="/images/meet1.jpg" alt="Second slide" />
-                        </div>
-                        <div className="carousel-item">
-                            <img className="d-block w-100" src="/images/meet1.jpg" alt="Third slide" />
-                        </div>
+                        {imageComponents}
                     </div>
-                    <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <a className="carousel-control-prev" href="#carouselComponent" role="button" data-slide="prev">
                         <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                         <span className="sr-only">Previous</span>
                     </a>
-                    <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <a className="carousel-control-next" href="#carouselComponent" role="button" data-slide="next">
                         <span className="carousel-control-next-icon" aria-hidden="true"></span>
                         <span className="sr-only">Next</span>
                     </a>

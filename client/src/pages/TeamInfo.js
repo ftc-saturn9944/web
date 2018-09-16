149eead6662530f855a4f8abcca9f1c135eb3ff4
Michael Privat <mprivat@majorspot.com>
2018-09-15 20:50:42 -0400

    Made to work better on mobile

--- client/src/pages/TeamInfo.js
+++ client/src/pages/TeamInfo.js
@@ -11,7 +11,7 @@ class TeamInfo extends Component {
             carouselIndicators.push(<li data-target="#carouselComponent" data-slide-to={index} key={index} className={index === 0 ? "active" : "other"}></li>);
             imageComponents.push(
                 <div className={`carousel-item${index === 0 ? " active" : ""}`}>
-                    <img className="d-block mx-auto" src={`/images/${name}`} max-width="100%" max-height="100%" alt="" />
+                    <img className="img-fluid mx-auto d-block" src={`/images/${name}`} max-width="100%" max-height="100%" alt="" />
                 </div>
             );
         });

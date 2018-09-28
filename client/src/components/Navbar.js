043a7eb5b8f3cefa77f936deacfc7abfff7058be
1234ViperSting <josephdelcastillo@outlook.com>
2018-09-27 22:04:43 -0400

    Made the NavBar prettier, but cant clear localStorage for some reason.

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -31,38 +31,38 @@ class Navbar extends Component {
         const loggedIn = localStorage.getItem("authKey") !== null;
         console.log("Logged in:", loggedIn);
         return (
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark"> {/* I removed fixed-top because it was obscuring parts of the page. Though a fixed navbar would be good, until we get that fixed (hehe) we should keep it as is.*/}
-                <a className="navbar-brand" href="#/">
-                    <img src="/images/saturn-logo.png" alt="Logo" />
-                </a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item" id="team-info">
-                            <a className="nav-link" href="#/">Team Info<span className="sr-only">(current)</span></a>
+            <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+                <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item" id="team-info">
+                            <a class="nav-link" href="#/">Team Info</a>
                         </li>
-                        <li className="nav-item" id="media-kit">
-                            <a className="nav-link" href="#/media">Media Kit</a>
+                        <li class="nav-item" id="media-kit">
+                            <a class="nav-link" href="#/media">Media Kit</a>
                         </li>
-                        <li className="nav-item" id="sponsors">
-                            <a className="nav-link" href="#/sponsors">Sponsors</a>
+                        <li class="nav-item" id="sponsors">
+                            <a class="nav-link" href="#/sponsors">Sponsors</a>
                         </li>
-                        <li className="nav-item" id="point-counter">
-                            <a className="nav-link" href="#/pointcounter">Points counter</a>
+                        <li class="nav-item" id="point-counter">
+                            <a class="nav-link" href="#/pointcounter">Point Counter</a>
                         </li>
-                        <li className="nav-item" id="registration">
-                            <a className="nav-link disabled" href="javascript:void;">Registration</a>
+                        <li class="nav-item" id="registration">
+                            <a class="nav-link" href="javascript:void;">Registration</a>
                         </li>
-                        <li className={"nav-item"+(loggedIn? " invisible" : "")} id="login">
-                            <a className="nav-link" href="#/login">Login</a>
-                        </li>
-
-
                     </ul>
-
+                </div>
+                <div class="mx-auto order-0">
+                    <img src="/images/saturn-logo.png" alt="Logo" />
+                </div>
+                <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+                    <ul class="navbar-nav ml-auto">
+                        <li class={"nav-item"+(loggedIn? "" : " invisible")} id="logoff">
+                            <a class="nav-link" href="#" onclick="localStorage.clear()">Log Off</a>
+                        </li>
+                        <li class={"nav-item"+(loggedIn? " invisible" : "")} id="login">
+                            <a class="nav-link" href="#/login">Log In</a>
+                        </li>
+                    </ul>
                 </div>
             </nav>
         );

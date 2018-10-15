f47e9ee7dbf8a194c1a1f0643f15c2fe78394a5e
Aylesbury <s08013237@students.duvalschools.org>
2018-10-15 15:46:13 -0400

    612

--- client/src/pages/MediaKit.js
+++ client/src/pages/MediaKit.js
@@ -13,14 +13,14 @@ class MediaKit extends Component {
                                 <div className="col-md-6">
                                     <div className="sponsor-card card">
                                         <div className="card-body">
-                                        <a className="btn btn-outline-dark" href="https://www.youtube.com/watch?v=TttTeH3yedw"><h3>Want to watch?</h3></a>
+                                        <a className="btn btn-outline-dark" href="https://www.youtube.com/watch?v=TttTeH3yedw"><p>Want to watch?</p></a>
                                         <div className="card-text">
                                             <p>
                                                 One of our most diligent students has made the books. Joesph
                                                 Delcastillo has made the news for being the STEM student of the week.
                                             </p>
                                             <div className="text-left">
-                                            <a className="btn btn-outline-dark" href="http://www.ftcroot.com/teams/9944/SATURN"><h3>Past Team Scores</h3></a>
+                                            <a className="btn btn-outline-dark" href="http://www.ftcroot.com/teams/9944/SATURN"><p>Past Team Scores</p></a>
                                             </div>
                                         </div>
                                         </div>

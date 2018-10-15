8d4dff896edf8f75f9a7437e0936e6be14974d9d
Hawes <s11638053@students.duvalschools.org>
2018-10-15 15:06:09 -0400

    Someone

--- client/src/pages/MediaKit.js
+++ client/src/pages/MediaKit.js
@@ -4,10 +4,23 @@ import Navbar from '../components/Navbar';
 class MediaKit extends Component {
     render() {
         return (
-            <div className="App">
-                <Navbar page="media-kit" />
+                <div className="App">
+                <Navbar page="media-kit" />'
+                    <div className="container-fluid">
+                        <div className="card-holder">
+                        <h2>Media Kit</h2>
+                            <div className="row">
+                                <div className="col-md-7">
+                                    <div className="sponsor-card card">
+                                        <div className="card-body">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 Media Kit
-            </div>
+                </div>
         );
     }
 }

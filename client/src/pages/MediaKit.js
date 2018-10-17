cfd5fb7077db7de4d9186c9a0feabb421ece02b2
Your Name <612billyn@gmail.com>
2018-10-17 18:47:34 -0400

    612

--- client/src/pages/MediaKit.js
+++ client/src/pages/MediaKit.js
@@ -7,7 +7,6 @@ class MediaKit extends Component {
                 <div className="App">
                 <Navbar page="media-kit" />'
                     <div className="container-fluid">
-
                         <div className="card-holder">
                         <h2>Look what we have done!</h2>
                             <div className="row">
@@ -16,19 +15,18 @@ class MediaKit extends Component {
                                         <div className="card-body">
                                         <div className="card-text"></div>
                                             <p>
-                                                One of our most diligent students has made the books. Joesph
-                                                Delcastillo has made the news for being the STEM student of the week.
+                                                Here is one of our best matches from a previous year.
                                             </p>
                                             <div className="text-left">
-                                        <a className="sponsor-name" href="https://www.youtube.com/watch?v=TttTeH3yedw"><h3>Want to watch?</h3></a>
+                                        <a className="btn btn-outline-dark" href="https://www.youtube.com/watch?v=TttTeH3yedw"><h3>Want to watch?</h3></a>
                                         
                                         <div className="card-text">
                                             <p>
-                                                One of our most diligent students has made the books. Joesph
-                                                Delcastillo has made the news for being the STEM student of the week.
+                                                If you want to check out some statistics from the past few years of SATURN. 
+                                                You can click the button.
                                             </p>
                                             <div className="text-left">
-                                            <a className="sponsor-name" href="http://www.ftcroot.com/teams/9944/SATURN"><h3>Past Team Scores</h3></a>
+                                            <a className="btn btn-outline-dark" href="http://www.ftcroot.com/teams/9944/SATURN"><h3>Past Team Scores</h3></a>
                                             </div>
                                         </div>
                                         </div>
